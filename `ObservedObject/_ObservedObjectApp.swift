83806d0891506c8c886facbeaa9d8c9a3126a11a
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-19 14:22:26 +0900

    Initial Commit

---
+++ `ObservedObject/_ObservedObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ObservedObjectApp.swift
+//  `ObservedObject
+//
+//  Created by 木村朋広 on 2024/02/19.
+//
+
+import SwiftUI
+
+@main
+struct _ObservedObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
